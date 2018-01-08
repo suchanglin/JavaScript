6f2075fb3eb1684dec413a36ba56642bf6c3cea0
AllySu <18795397123@163.com>
2018-01-08 20:31:27 +0800

    myspace

--- 9-1nineth/眼睛在线试戴/eyeglass.js
+++ 9-1nineth/眼睛在线试戴/eyeglass.js
@@ -2,21 +2,21 @@
 * @Author: Administrator
 * @Date:   2018-01-06 15:51:39
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-01-06 15:55:54
+* @Last Modified time: 2018-01-08 20:30:20
 */
-var fgm = {};
+var mySpace = {};
 
-fgm.$ = function(id) {
+mySpace.$ = function(id) {
   return typeof id === "object" ? id : document.getElementById(id)
 };
 
-fgm.$$ = function(tagName, oParent) {
+mySpace.$$ = function(tagName, oParent) {
   return (oParent || document).getElementsByTagName(tagName)
 };
 
-fgm.$$$ = function(className, tagName, oParent) {
+mySpace.$$$ = function(className, tagName, oParent) {
   var reg = new RegExp("(^|\\s)" + className + "(\\s|$)"),
-    aEl = fgm.$$(tagName || "*", oParent)
+    aEl = mySpace.$$(tagName || "*", oParent)
     len = aEl.length,
     aClass = [],
     i = 0;
@@ -24,13 +24,13 @@ fgm.$$$ = function(className, tagName, oParent) {
   return aClass
 };
 
-fgm.index = function(element) {
+mySpace.index = function(element) {
   var aChild = element.parentNode.children;
   for(var i = aChild.length; i--;)
     if(element == aChild[i]) return i
 };
 
-fgm.css = function(element, attr, value) {
+mySpace.css = function(element, attr, value) {
   if(arguments.length == 2) {
     var style = element.style,
       currentStyle = element.currentStyle;
@@ -68,7 +68,7 @@ fgm.css = function(element, attr, value) {
   }
 };
 
-fgm.attr = function(element, attr, value) {
+mySpace.attr = function(element, attr, value) {
   if(arguments.length == 2) {
     return element.attributes[attr] ? element.attributes[attr].nodeValue : undefined
   }
@@ -77,7 +77,7 @@ fgm.attr = function(element, attr, value) {
   }
 };
 
-fgm.contains = function(element, oParent) {
+mySpace.contains = function(element, oParent) {
   if(oParent.contains) {
     return oParent.contains(element)
   }
@@ -86,7 +86,7 @@ fgm.contains = function(element, oParent) {
   }
 };
 
-fgm.isParent = function(element, tagName) {
+mySpace.isParent = function(element, tagName) {
   while(element != undefined && element != null && element.tagName.toUpperCase() !== "BODY") {
     if(element.tagName.toUpperCase() == tagName.toUpperCase())
       return true;
@@ -95,14 +95,14 @@ fgm.isParent = function(element, tagName) {
   return false
 };
 
-fgm.extend = function(destination, source) {
+mySpace.extend = function(destination, source) {
   for (var property in source) destination[property] = source[property];
   return destination
 };
 
-fgm.ajax = function(config) {
+mySpace.ajax = function(config) {
   var oAjax = null,
-  config = fgm.extend({
+  config = mySpace.extend({
     cache: !0,
     param: "",
     type: "GET",
@@ -124,10 +124,10 @@ fgm.ajax = function(config) {
   oAjax.send(config.type.toUpperCase() === "POST" && config.param || null)
 };
 
-fgm.animate = function(obj, json, opt) {
+mySpace.animate = function(obj, json, opt) {
   clearInterval(obj.timer);
   obj.iSpeed = 0;
-  opt = fgm.extend({
+  opt = mySpace.extend({
     type: "buffer",
     callback: function() {}
   }, opt);
@@ -137,43 +137,43 @@ fgm.animate = function(obj, json, opt) {
       property = null,
       maxSpeed = 30;
     for(property in json) {
-      iCur = fgm.css(obj, property);
+      iCur = mySpace.css(obj, property);
       property == "opacity" && (iCur = parseInt(iCur.toFixed(2) * 100));
       switch(opt.type) {
         case "buffer":
           obj.iSpeed = (json[property] - iCur) / 5;
           obj.iSpeed = obj.iSpeed > 0 ? Math.ceil(obj.iSpeed) : Math.floor(obj.iSpeed);
-          json[property] == iCur || (complete = !1, fgm.css(obj, property, property == "zIndex" ? iCur + obj.iSpeed || iCur * -1 : iCur + obj.iSpeed));
+          json[property] == iCur || (complete = !1, mySpace.css(obj, property, property == "zIndex" ? iCur + obj.iSpeed || iCur * -1 : iCur + obj.iSpeed));
           break;
         case "flex":
           obj.iSpeed += (json[property] - iCur) / 5;
           obj.iSpeed *= 0.7;
           obj.iSpeed = Math.abs(obj.iSpeed) > maxSpeed ? obj.iSpeed > 0 ? maxSpeed : -maxSpeed : obj.iSpeed;
-          Math.abs(json[property] - iCur) <=1 && Math.abs(obj.iSpeed) <= 1 || (complete = !1, fgm.css(obj, property, iCur + obj.iSpeed));
+          Math.abs(json[property] - iCur) <=1 && Math.abs(obj.iSpeed) <= 1 || (complete = !1, mySpace.css(obj, property, iCur + obj.iSpeed));
           break;
       }
     }
     if(complete) {
       clearInterval(obj.timer);
-      if(opt.type == "flex") for(property in json) fgm.css(obj, property, json[property]);
+      if(opt.type == "flex") for(property in json) mySpace.css(obj, property, json[property]);
       opt.callback.apply(obj, arguments);
     }
   }, 30)
 };
 
 function complete() {
-  var oBox = fgm.$("box"),
-    oLeft = fgm.$("left"),
-    oTools = fgm.$("tools"),
+  var oBox = mySpace.$("box"),
+    oLeft = mySpace.$("left"),
+    oTools = mySpace.$("tools"),
     oPhoto = null,
     oGlass = null,
-    oNormal = fgm.$$$("normal")[0],
-    oActive = fgm.$$$("active")[0],
-    oContent = fgm.$$$("content")[0],
-    oRight = fgm.$("right"),
-    oLMask = fgm.$$$("mask", "div", oLeft)[0],
-    oRMask = fgm.$$$("mask", "div", oRight)[0],
-    oGlassList = fgm.$$$("glassList")[0],
+    oNormal = mySpace.$$$("normal")[0],
+    oActive = mySpace.$$$("active")[0],
+    oContent = mySpace.$$$("content")[0],
+    oRight = mySpace.$("right"),
+    oLMask = mySpace.$$$("mask", "div", oLeft)[0],
+    oRMask = mySpace.$$$("mask", "div", oRight)[0],
+    oGlassList = mySpace.$$$("glassList")[0],
     zIndex = 0;
 
   //事件委托
@@ -183,22 +183,22 @@ function complete() {
 
     //筛选
     (function() {
-      if(oTarget.tagName.toUpperCase() === "A" && fgm.contains(oTarget, oNormal)) {
-        var oUl = fgm.$$("ul", oActive)[0],
+      if(oTarget.tagName.toUpperCase() === "A" && mySpace.contains(oTarget, oNormal)) {
+        var oUl = mySpace.$$("ul", oActive)[0],
           iW = oActive.offsetWidth,
-          iL = oTarget.parentNode.offsetLeft - fgm.css(oActive, "marginLeft"),
-          maxH = fgm.css(oActive, "height"),
-          minH = fgm.css(oActive, "lineHeight");
-        fgm.animate(oActive, {height:minH}, {
+          iL = oTarget.parentNode.offsetLeft - mySpace.css(oActive, "marginLeft"),
+          maxH = mySpace.css(oActive, "height"),
+          minH = mySpace.css(oActive, "lineHeight");
+        mySpace.animate(oActive, {height:minH}, {
           callback: function() {
-            fgm.animate(oUl, {left:-iL});
-            fgm.animate(oActive, {left:iL}, {
+            mySpace.animate(oUl, {left:-iL});
+            mySpace.animate(oActive, {left:iL}, {
               callback: function() {
-                fgm.animate(oActive, {height:maxH}, {
+                mySpace.animate(oActive, {height:maxH}, {
                   callback: function() {
                     var i = 0,
-                      aLi = fgm.$$("li", oContent),
-                      sType = fgm.attr(oTarget, "data-type"),
+                      aLi = mySpace.$$("li", oContent),
+                      sType = mySpace.attr(oTarget, "data-type"),
                       aType = function() {
                         if(sType === "*") {
                           for(var i = aLi.length, a = []; i--;) a[i] = i;
@@ -206,8 +206,8 @@ function complete() {
                         }
                         return sType.split(",")
                       }();
-                    for(i = aLi.length; i--;) fgm.css(aLi[i], {opacity:20}), aLi[i].className = "hidden";
-                    for(i = aType.length; i--;) fgm.animate(aLi[aType[i]], {opacity:100}), aLi[aType[i]].className = "active"
+                    for(i = aLi.length; i--;) mySpace.css(aLi[i], {opacity:20}), aLi[i].className = "hidden";
+                    for(i = aType.length; i--;) mySpace.animate(aLi[aType[i]], {opacity:100}), aLi[aType[i]].className = "active"
                   }
                 })
               }
@@ -218,18 +218,18 @@ function complete() {
     })();
 
     //模特选择
-    if(oTarget.tagName.toUpperCase() === "IMG" && oTarget.parentNode.className !== "hidden" && fgm.contains(oTarget, oContent)) {
-      fgm.animate(oLMask, {opacity:100, zIndex:1}, {
+    if(oTarget.tagName.toUpperCase() === "IMG" && oTarget.parentNode.className !== "hidden" && mySpace.contains(oTarget, oContent)) {
+      mySpace.animate(oLMask, {opacity:100, zIndex:1}, {
         callback: function() {
           oPhoto = document.createElement("img");
           oPhoto.src = oTarget.src.replace("model", "big");
           oPhoto.className = "photo";
           oLeft.insertBefore(oPhoto, oLMask);
-          fgm.animate(oPhoto, {width:428, height:526, top:0, left:0}, {
+          mySpace.animate(oPhoto, {width:428, height:526, top:0, left:0}, {
             callback: function() {
-              fgm.animate(oTools, {top:8, left:385}, {
+              mySpace.animate(oTools, {top:8, left:385}, {
                 callback: function() {
-                  fgm.animate(oRMask, {opacity:0, zIndex:-1})
+                  mySpace.animate(oRMask, {opacity:0, zIndex:-1})
                 }
               })
             }
@@ -245,25 +245,25 @@ function complete() {
         case "open":
           oTarget.className = "close";
           oTarget.innerHTML = "\u5c55\u5f00";
-          fgm.animate(oTools, {height:33});
+          mySpace.animate(oTools, {height:33});
           break;
         case "close":
           oTarget.className = "open";
           oTarget.innerHTML = "\u6536\u7f29";
-          fgm.animate(oTools, {height:173});
+          mySpace.animate(oTools, {height:173});
           break;
         case "reset":
           oGlass && oGlass.parentNode.removeChild(oGlass), oGlass = null;
-          fgm.animate(oRMask, {opacity:70, zIndex:++zIndex});
-          fgm.animate(oPhoto, {left:-530}, {
+          mySpace.animate(oRMask, {opacity:70, zIndex:++zIndex});
+          mySpace.animate(oPhoto, {left:-530}, {
             callback: function() {
               this.parentNode.removeChild(this);
-              ++count == 2 && fgm.animate(oLMask, {opacity:0, zIndex:-1})
+              ++count == 2 && mySpace.animate(oLMask, {opacity:0, zIndex:-1})
             }
           });
-          fgm.animate(oTools, {top:-30, left:-55}, {
+          mySpace.animate(oTools, {top:-30, left:-55}, {
             callback: function() {
-              ++count == 2 && fgm.animate(oLMask, {opacity:0, zIndex:-1})
+              ++count == 2 && mySpace.animate(oLMask, {opacity:0, zIndex:-1})
             }
           });
           break;
@@ -275,8 +275,8 @@ function complete() {
       var oImg = null,
         aLI = null,
         i = 0;
-      if(fgm.contains(oTarget, oGlassList) && fgm.isParent(oTarget, "li")) {
-        for(aLi = fgm.$$("li", oGlassList), i = aLi.length; i--;) aLi[i].className = "";
+      if(mySpace.contains(oTarget, oGlassList) && mySpace.isParent(oTarget, "li")) {
+        for(aLi = mySpace.$$("li", oGlassList), i = aLi.length; i--;) aLi[i].className = "";
         oGlass && oGlass.parentNode.removeChild(oGlass), oGlass = null;
         oGlass = document.createElement("div");
         oImg = document.createElement("img");
@@ -288,11 +288,11 @@ function complete() {
               break;
             case "LI":
               oTarget.className = "current";
-              return fgm.$$("img", oTarget)[0].src.replace(".jpg", ".png");
+              return mySpace.$$("img", oTarget)[0].src.replace(".jpg", ".png");
               break;
             default:
               oTarget.parentNode.className = "current";
-              return fgm.$$("img", oTarget.parentNode)[0].src.replace(".jpg", ".png")
+              return mySpace.$$("img", oTarget.parentNode)[0].src.replace(".jpg", ".png")
           }
         }();
         oGlass.className = "glass";
@@ -314,7 +314,7 @@ function complete() {
             iT < 0 && (iT = 0);
             iL > maxL && (iL = maxL);
             iT > maxT && (iT = maxT);
-            fgm.css(oGlass, {top:iT + "px", left: iL + "px"});
+            mySpace.css(oGlass, {top:iT + "px", left: iL + "px"});
             return false
           };
           document.onmouseup = function() {
@@ -330,8 +330,8 @@ function complete() {
 
     //模拟select
     (function() {
-      var oSearch = fgm.$$$("search")[0],
-        aUl = fgm.$$("ul", oSearch),
+      var oSearch = mySpace.$$$("search")[0],
+        aUl = mySpace.$$("ul", oSearch),
         oUl = null,
         aLi = null,
         oSelect = null,
@@ -339,24 +339,24 @@ function complete() {
         i = 0;
       switch(oTarget.className) {
         case "select":
-          for(i = aUl.length; i--;) fgm.css(aUl[i], {display:"none"});
-          oUl = fgm.$$("ul", oTarget.parentNode)[0];
-          fgm.css(oUl, {display:"block"});
-          fgm.css(oTarget.parentNode, {zIndex:++zIndex});
+          for(i = aUl.length; i--;) mySpace.css(aUl[i], {display:"none"});
+          oUl = mySpace.$$("ul", oTarget.parentNode)[0];
+          mySpace.css(oUl, {display:"block"});
+          mySpace.css(oTarget.parentNode, {zIndex:++zIndex});
           document.onclick = function() {
             this.onclick = null;
-            fgm.css(oUl, {display:"none"})
+            mySpace.css(oUl, {display:"none"})
           };
           e.cancelBubble = true;
           break;
         case "btn":
-          param = fgm.attr(fgm.$$$("selectWrap")[0], "data-param");
+          param = mySpace.attr(mySpace.$$$("selectWrap")[0], "data-param");
           if(param == undefined) {
             alert("请选择品牌！")
           }
           else {
             oGlassList.innerHTML = "";
-            fgm.ajax({
+            mySpace.ajax({
               url: "data.asp?code=" + param,
               cache: !1,
               success: function(data) {
@@ -366,13 +366,13 @@ function complete() {
           }
           break;
         default:
-          if(oTarget.tagName.toUpperCase() == "A" && fgm.contains(oTarget, oSearch) && fgm.isParent(oTarget, "LI")) {
-            aLi = fgm.$$("li", oTarget.parentNode.parentNode);
+          if(oTarget.tagName.toUpperCase() == "A" && mySpace.contains(oTarget, oSearch) && mySpace.isParent(oTarget, "LI")) {
+            aLi = mySpace.$$("li", oTarget.parentNode.parentNode);
             for(i = aLi.length; i--;) aLi[i].className = "";
             oTarget.parentNode.className = "current";
-            oSelect = fgm.$$$("select", "a" ,oTarget.parentNode.parentNode.parentNode)[0];
+            oSelect = mySpace.$$$("select", "a" ,oTarget.parentNode.parentNode.parentNode)[0];
             oSelect.innerHTML = oTarget.innerHTML;
-            fgm.attr(oSelect.parentNode, "data-param", fgm.index(oTarget.parentNode))
+            mySpace.attr(oSelect.parentNode, "data-param", mySpace.index(oTarget.parentNode))
           }
       }
     })()
